428428282e0619aaad4c2b5b57050e7b8ef61f72
rtgc0915 <rtgc0915@gmail.com>
2024-07-24 19:33:16 +0900

    initail commit

---
+++ src/main/java/com/ch_eatimg/ch_eating/ChEatingApplication.java
@@ -0,0 +1,13 @@
+package com.ch_eatimg.ch_eating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChEatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChEatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ch_eatimg/ch_eating/ChEatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch_eatimg.ch_eating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChEatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
