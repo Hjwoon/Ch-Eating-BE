502abfc8ebc642dc6fcc892ab3d274680ac8b918
Hjwoon <hjw030705@naver.com>
2024-08-01 13:20:13 +0900

    ♻️ 식사량 조회 API의 Response에 createAt, updateAt 추가

--- src/main/java/com/ch_eatimg/ch_eating/meal/dto/MealListDto.java
+++ src/main/java/com/ch_eatimg/ch_eating/meal/dto/MealListDto.java
@@ -2,20 +2,21 @@ package com.ch_eatimg.ch_eating.meal.dto;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MealListDto {
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class MealResponse {
 
-        private Long mealId; // 식사량 ID
-        private String mealType; // 식사 종류
-        private String mealBrand; // 식사 브랜드명
-        private String mealName; // 식사 메뉴명
-        private String mealAmount; // 식사량
-        private String mealDetail; // 세부사항
-
-    }
+    private Long mealId; // 식사량 ID
+    private String mealType; // 식사 종류
+    private String mealBrand; // 식사 브랜드명
+    private String mealName; // 식사 메뉴명
+    private String mealAmount; // 식사량
+    private String mealDetail; // 세부사항
+    private String createAt; // create-At
+    private String updateAt; // update-At
 }
