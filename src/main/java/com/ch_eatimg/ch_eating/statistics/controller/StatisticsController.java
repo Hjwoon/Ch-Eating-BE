08525644821eadd0d6f22c2ffb01b80529ddcd8a
Hjwoon <hjw030705@naver.com>
2024-08-01 15:33:01 +0900

    ♻️ 통계 세부 조회 관련 API에 유저 아이디 일치 로직 추가

--- src/main/java/com/ch_eatimg/ch_eating/statistics/controller/StatisticsController.java
+++ src/main/java/com/ch_eatimg/ch_eating/statistics/controller/StatisticsController.java
@@ -4,12 +4,11 @@ import com.ch_eatimg.ch_eating.statistics.dto.StatisticsMonthlyResponseDto;
 import com.ch_eatimg.ch_eating.statistics.dto.StatisticsResponseDto;
 import com.ch_eatimg.ch_eating.statistics.service.StatisticsService;
 import com.ch_eatimg.ch_eating.util.response.CustomApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/statistics")
@@ -21,17 +20,21 @@ public class StatisticsController {
     // 주간 조회(요일별)
     @GetMapping("/weekly")
     public ResponseEntity<CustomApiResponse<StatisticsResponseDto>> getFakeHungerStatistics(
+            HttpServletRequest request, @Valid @RequestBody StatisticsResponseDto dto,
             @RequestParam String startDate,
             @RequestParam String endDate) {
-        return statisticsService.getFakeHungerStatistics(startDate, endDate);
+        CustomApiResponse<StatisticsResponseDto> result = statisticsService.getFakeHungerStatistics(request, dto, startDate, endDate);
+        return ResponseEntity.status(result.getStatus()).body(result);
     }
 
     // 월간 조회(한 주씩 합산)
     @GetMapping("/monthly")
     public ResponseEntity<CustomApiResponse<StatisticsMonthlyResponseDto>> getFakeHungerStatisticsMonth(
+            HttpServletRequest request, @Valid @RequestBody StatisticsMonthlyResponseDto dto,
             @RequestParam String startDate,
             @RequestParam String endDate) {
-        return statisticsService.getFakeHungerStatisticsMonth(startDate, endDate);
+        CustomApiResponse<StatisticsMonthlyResponseDto> result = statisticsService.getFakeHungerStatisticsMonth(request, dto, startDate, endDate);
+        return ResponseEntity.status(result.getStatus()).body(result);
     }
 
 }
